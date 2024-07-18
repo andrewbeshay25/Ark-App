ea596e5363bbdd65c0ba6c31c9902e6dd5c0015b
Andrew <andrewbeshay2525@gmail.com>
2024-07-17 21:03:46 -0400

    Initial Commit

---
+++ ARK/ARKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKApp.swift
+//  ARK
+//
+//  Created by Andrew Beshay on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ARKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARK
+//
+//  Created by Andrew Beshay on 7/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
